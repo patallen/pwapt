a996367f65167ce9dcf41a5ed308c35d4be185b0
Patrick Allen <prallen90@gmail.com>
2017-02-12 21:19:06 -0500

    Better document the Fake* classes

--- tests/mock.py
+++ tests/mock.py
@@ -1,50 +1,35 @@
 
-
 class FakeCode(object):
+    """Mock for the python core's Code object.
+
+    Used to store information on the underlying code of a Frame.
+    """
+
     def __init__(self, co_filename, co_name):
         self.co_filename = co_filename
         self.co_name = co_name
 
 
 class FakeFrame(object):
+    """Mock for python core's Frame object.
+
+    Holds information on the current stack frame. We can easily
+    back into an entire call stack by looping on Frame.f_back.
+    """
+
     def __init__(self, f_code, f_globals, previous):
         self.f_code = f_code
         self.f_globals = f_globals
         self.f_back = previous
 
 
-EXAMPLE_FRAME_INFOS = [
-    {
-        "f_code": {
-            "co_filename": "/you/are/such/a/sillyboy.py",
-            "co_name": "get_silly"
-        },
-        "f_globals": {
-            "__name__": "sillyboy"
-        }
-    },
-    {
-        "f_code": {
-            "co_name": "sparkle",
-            "co_filename": "/i/love/seltzer.py"
-        },
-        "f_globals": {
-            "__name__": "seltzer"
-        }
-    },
-    {
-        "f_code": {
-            "co_name": "win",
-            "co_filename": "/breakfast/of/champions.py"
-        },
-        "f_globals": {
-            "__name__": "champions"
-        }
-    }
-]
-
-
 def make_fake_code(code_info):
+    """Build a FakeCode.
+
+    :param code_info: <dict> Contains:
+        - co_name (name of the calling function)
+        - co_filename (name of the module in which the function resides)
+    """
     return FakeCode(
         co_filename=code_info.get('co_filename'),
         co_name=code_info.get('co_name')
@@ -54,9 +39,11 @@ def make_fake_code(code_info):
 def make_fake_frame(frame_info, previous_frame=None):
     """Build a FakeFrame.
 
-    :param frame_info: <dict> Containing info on f_code and f_globals.
-    :param previous_frame: <FakeFrame> 
-    :returns: <FakeFrame> The highest-level FakeFrame in the callstack.
+    :param frame_info: <dict> Contains:
+        - f_code (FakeCode object)
+        - f_globals (dict of all globals captured)
+    :param previous_frame: <FakeFrame>
+    :returns: <FakeFrame>
     """
     code_info = frame_info.get('f_code')
     globs = frame_info.get('f_globals')
@@ -76,5 +63,3 @@ def make_fake_frames(frame_infos):
         frame = make_fake_frame(frame_inf, previous_frame=previous)
         previous = frame
     return frame
-
-
