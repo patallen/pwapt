45bb9898a82e4542e7bd15640c4d31c3b3463d33
Patrick Allen <prallen90@gmail.com>
2017-02-15 23:55:18 -0500

    Add import_from_string helper for importing middleware

--- pwapt/lib.py
+++ pwapt/lib.py
@@ -1,4 +1,5 @@
 """Helper classes and functions."""
+from importlib import import_module
 
 
 class cachedproperty(object):
@@ -30,3 +31,23 @@ class cachedproperty(object):
         rv = self.func(obj)
         self._prop_cache(obj)[self.f_name] = rv
         return rv
+
+
+def import_from_string(dotted_path):
+    """
+    Import a dotted module path and return the attribute/class designated by
+    the last name in the path. Raise ImportError if the import failed.
+    """
+    try:
+        module_path, class_name = dotted_path.rsplit('.', 1)
+    except ValueError:
+        raise ImportError("Not a module path.")
+
+    module = import_module(module_path)
+
+    try:
+        return getattr(module, class_name)
+    except AttributeError:
+        raise ImportError('Module "%s" does not define a "%s"' % (
+            module_path, class_name)
+        )
