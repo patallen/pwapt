cd3af690966817c7b1e71e1d572fb39e05a4b16f
Patrick Allen <prallen90@gmail.com>
2017-03-02 23:50:15 -0500

    Add xml output using pytest

--- runtests_junit.sh
+++ runtests_junit.sh
@@ -1,1 +1,8 @@
-pytest --junitxml=tests_junit
+#!/bin/bash
+set -e
+
+. ~/.virtualenvs/python2.7/bin/activate
+
+pip install -r requirements/testing.txt
+
+py.test --junitxml junit_test_output/results_$(date +"%Y%m%d%H%M").xml
